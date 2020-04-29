ad68330d62182d84abe1ed5dbdc86b9bd5eba049
Hans Dembinski <hans.dembinski@gmail.com>
2020-04-29 16:01:53 +0200

    updates and fixes

--- crdb.py
+++ crdb.py
@@ -1,42 +1,60 @@
-import requests
+import urllib.request as rq
 import numpy as np
+import warnings
 
 
-def query(num=None, energy_type="EK", **kwargs):
+def query(num, den=None, energy_type="R", url=None, **kwargs):
     """
     Queries the CRDB and returns the table as a numpy array.
     
-    Parameters are passed as keywords directly to this function.
+    Parameters are passed as keywords directly to this function. All values are
+    case insensitive.
     
     Parameters
     ----------
     num: str
         Element, isotope, or particle.
-    energy_type: str (default: EK)
+    energy_type: str (optional, default: "R")
         Energy unit for the requested quantity. Valid values: EKN, EK, R, ETOT.
+    url: str (optional, default: None)
+        URL to send the request to, defaults to the standard url. This is an expert
+        option, users do not need to change this.
+    kwargs: 
+        Other keyword-value pairs are interpreted as parameters for the query.
+        See http://lpsc.in2p3.fr/crdb for a documentation which parameters are accepted.
 
-    Other keyword-value pairs are interpreted as parameters for the query. See
-    http://lpsc.in2p3.fr/crdb for a documentation which parameters are accepted.
-
-    Passing an unknown parameter or using an unknown value results in an a ValueError.
+    Passing an unknown parameter or using an unknown value triggers a ValueError.
 
     Returns
     -------
     numpy record array with the database content
     """
-    if num is None:
-        raise KeyError("setting parameter `num` is required")
+    # "+" must be escaped in URL, see
+    # https://en.wikipedia.org/wiki/Percent-encoding
+    if num.lower() == "e+":
+        num = "e%2B"
+
+    # workaround for empty error message from CRDB
     valid_energy_types = ("EKN", "EK", "R", "ETOT")
     if energy_type.upper() not in valid_energy_types:
         raise ValueError("energy_type must be one of " + ",".join(valid_energy_types))
+
+    # do the query
     kwargs["num"] = num
     kwargs["energy_type"] = energy_type.upper()
-    urlcmd = "http://lpsc.in2p3.fr/crdb/rest.php?" + "&".join(
-        ["{0}={1}".format(k, v) for (k, v) in kwargs.items()]
+    urlcmd = (
+        (url or "http://lpsc.in2p3.fr/crdb")
+        + "/rest.php?"
+        + "&".join(["{0}={1}".format(k, v) for (k, v) in kwargs.items()])
     )
-    data = requests.get(urlcmd).content.decode("utf-8").split("\n")
+    with rq.urlopen(urlcmd, timeout=5) as u:
+        data = u.read().decode("utf-8").split("\n")
+
+    # check for errors and display them
     if len(data) == 1:
         raise ValueError(data[0])
+
+    # convert text to numpy record array
     fields = [
         ("quantity", "U10"),
         ("sub_exp", "U100"),
@@ -55,4 +73,35 @@ def query(num=None, energy_type="EK", **kwargs):
         ("datetime", "U100"),
         ("is_upper_limit", "?"),
     ]
-    return np.genfromtxt(data, fields)
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        return np.genfromtxt(data, fields)
+
+
+def test_query():
+    import pytest
+
+    tab = query("H")
+    assert len(tab) > 1
+
+    tab = query("e+")
+    assert len(tab) > 1
+
+    tab = query("e-")
+    assert len(tab) > 1
+
+    tab = query("B", "C")
+    assert len(tab) > 1
+
+    with pytest.raises(ValueError):
+        query("Foobar")
+
+    with pytest.raises(ValueError):
+        query("H", energy_type="Foobar")
+
+
+if __name__ == "__main__":
+    import pytest
+
+    pytest.main([__file__])
